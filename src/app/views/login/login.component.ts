1e3f408af2f03f8957e95b784c7404fbd2c64aca
julia0bandeira <juliabandeira2004@gmail.com>
2022-12-16 17:09:19 -0300

    GameMania

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,27 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor(private loginService:LoginService) { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
-    userModel = new User ();
+  userModel = new User();
 
-    receberDados() {
-        this.loginService.login(this.userModel).subscribe((response) => {
-          console.log(response)}, (erro) => {
-          console.log(erro.error)
-          }
-            )
-      
+  onSubmit() {
+    console.log("Modelo:", this.loginModel)
+
+    const listaPalavras: string[] = ["select ", "from ", "drop ", "or ", "having ", "group ", "by ", "insert ", "exec ", "\"", "\'", "--", "#", "*",]
+
+    listaPalavras.forEach(palavra => {
+      if (this.loginModel.email.toLowerCase().includes(palavra)) {
+        this.userModel.email = "";
+        alert("SQL injection detectado!");
+
+      }
+    })
+
+  }
+
+  receberDados() {
+    console.log(this.userModel)
+
+    this.loginService.login(this.userModel).subscribe((response) => {
+      console.log("Deu certo")
+      this.router.navigateByUrl("/")
+
+    }, (erro) => {
+      console.log("Deu erro")
     }
+
+    )
+
+  }
+
+  loginModel(arg0: string, loginModel: any) {
+    throw new Error('Method not implemented.');
+  }
+
 
 }
